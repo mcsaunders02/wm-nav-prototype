f45f8e198ac033b15ab4f37baa9c86545150442f
milesman34 <milesman034@gmail.com>
2024-11-05 14:45:00 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the WM Nav Prototype. Open a dev server with `npm run dev`. Build with `npm run build`, the resulting index.html file is in the dist folder.
+
+Use `npm run install` to install the needed dependencies.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+export const App = () => {
+    return (
+        <div className="main">
+            <div className="phone-container">
+                <div className="center-horizontal">
+                    <div className="phone-camera"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
