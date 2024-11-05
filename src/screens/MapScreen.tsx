55b7e02c153d66a95bad7a333e64bfeedd6cf54e
mcsaunders02 <mcsaunders02@wm.edu>
2024-11-05 18:29:25 -0500

    Exit route button so far

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -8,13 +8,27 @@ export const MapScreen = () => {
     return (
         <div className="screen map-screen">
             {/* This actually serves as a button, so you click on it and get taken to the new page */}
-            <div
-                className="map-destination-input"
-                onClick={() => dispatch(setScreen(AppScreen.SelectDestination))}
-            >
-                towards Destination
+            <div className="one-container">
+                <div
+                    className="map-destination-input"
+                    onClick={() => dispatch(setScreen(AppScreen.SelectDestination))}
+                >
+                    towards Destination
+                </div>
+                <img className="search-icon" src="./search.svg" alt="Search"></img>
             </div>
-            <img className="search-icon" src="./search.svg" alt="Search"></img>
+
+            <div className="next-direction-bar">
+                <img src="./leftarrow.svg" alt="Left Arrow"></img>
+
+                <div className="next-direction">
+                    200 ft at Landrum Dr
+                </div>
+
+                <div className="exit-route-button">
+                    Exit
+                </div>
+            </div>
         </div>
     );
 };
