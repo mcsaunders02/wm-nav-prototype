f06d92eb42fa576c6c87bcd28b9ba20b5e71b3e4
mcsaunders02 <mcsaunders02@wm.edu>
2024-11-05 18:21:08 -0500

    Towards destination bar

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,6 +1,20 @@
+import { useDispatch } from "react-redux";
+import { AppScreen, setScreen } from "../redux/mainSlice";
+
 // This screen shows the map
 export const MapScreen = () => {
-    return <div className="screen map-screen">
-        Map
-    </div>
-}
+    const dispatch = useDispatch();
+
+    return (
+        <div className="screen map-screen">
+            {/* This actually serves as a button, so you click on it and get taken to the new page */}
+            <div
+                className="map-destination-input"
+                onClick={() => dispatch(setScreen(AppScreen.SelectDestination))}
+            >
+                towards Destination
+            </div>
+            <img className="search-icon" src="./search.svg" alt="Search"></img>
+        </div>
+    );
+};
