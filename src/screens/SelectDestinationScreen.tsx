56b3d7b2ddbfe577110b76bedfebf20672e64a7a
mcsaunders02 <mcsaunders02@wm.edu>
2024-11-06 10:22:53 -0500

    Work on the select location inputs

--- src/screens/SelectDestinationScreen.tsx
+++ src/screens/SelectDestinationScreen.tsx
@@ -14,10 +14,16 @@ const SelectDest = ({
     autofocus?: boolean;
 }) => {
     const [inputText, setInputText] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         const listener = (event: KeyboardEvent) => {
-            if (event.key === "Enter") {
+            if (event.key === "Enter" && inputText.trim() !== "") {
+                if (document.activeElement instanceof HTMLElement) {
+                    document.activeElement.blur();
+                }
+
+                setIsOpen(false);
                 onSubmit(inputText);
             }
         };
@@ -30,17 +36,21 @@ const SelectDest = ({
     }, [onSubmit, text, inputText]);
 
     return (
-        <div className="one-container">
-            <input
-                id={`${text}-input`}
-                className="select-dest-input"
-                placeholder={text}
-                onSubmit={() => onSubmit(inputText)}
-                value={inputText}
-                onChange={(event) => setInputText(event.target.value)}
-                autoFocus={autofocus}
-            ></input>
-            <img className="search-icon" src="./search.svg" alt="Search"></img>
+        <div className="search-container-full">
+            <div className="one-container">
+                <input
+                    id={`${text}-input`}
+                    className="select-dest-input"
+                    placeholder={text}
+                    value={inputText}
+                    onChange={(event) => setInputText(event.target.value)}
+                    onFocus={() => setIsOpen(true)}
+                    autoFocus={autofocus}
+                ></input>
+                <img className="search-icon" src="./search.svg" alt="Search"></img>
+            </div>
+
+            {isOpen && <div className="search-container">Test</div>}
         </div>
     );
 };
@@ -53,15 +63,13 @@ export const SelectDestinationScreen = () => {
         <div className="screen select-dest-screen">
             <SelectDest
                 text="Select Destination"
-                onSubmit={(text) => {
-                    if (text.trim() !== "") {
-                        dispatch(setScreen(AppScreen.Map));
-                    }
+                onSubmit={(_) => {
+                    dispatch(setScreen(AppScreen.Map));
                 }}
                 autofocus={true}
             />
 
-            <SelectDest text="Current Location" />
+            <SelectDest text="Select Current Location" />
         </div>
     );
 };
